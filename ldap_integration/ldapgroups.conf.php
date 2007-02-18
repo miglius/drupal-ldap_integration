a88fe7ca64077616eb53522039937afcd8582dc0
kreaper <kreaper>
2007-02-18 14:28:24 +0000

    Bugfix for http://drupal.org/node/120249strip DNs into friendly names after running the groups through the filters

--- ldap_integration/ldapgroups.conf.php
+++ ldap_integration/ldapgroups.conf.php
@@ -13,30 +13,33 @@
 
 $GLOBALS['ldap_group_role_mappings'] = array(
   // LDAP group => Drupal role
-  'cn=users,ou=Group,dc=example,dc=com' => 'Users',
-  'cn=IT,ou=Group,dc=example,dc=com' => 'SiteAdmins'  
+  
+  // make sure the last group->role mapping does NOT have a trailing comma (,)
+  'cn=admin,ou=Group,dc=example,dc=com' => 'IT'
+  
 );
 
 // Note: Uncommenting this function will limit the groups -> roles conversion to ONLY those groups that are 
 // specified in the function. 
-/*
+
 function ldapgroups_roles_filter($groups) { 
   global $ldap_group_role_mappings; 
   $roles = array(); 
   // this should take the roles array, pass it thru the filters and send a NEW set of roles back the filter 
-  foreach ( $groups as $group ) { 
+  foreach ( $groups as $group ) {
     foreach ($ldap_group_role_mappings as $approved_group => $approved_role) {
-      // msg_r("matching $key to $role<br>");
-      if (strcasecmp($approved_group, $group) == 0) {
+       // must strip spaces ?
+       $group_stripped = preg_replace('/\s+/', '', $group);
+       $approved_group_stripped = preg_replace('/\s+/', '', $approved_group);       
+      if (strcasecmp($approved_group_stripped, $group_stripped) == 0) {
         // this role is specified -- grant
-        // msg_r("matched an approved group: $approved_group");
 	   $roles[] = $approved_role;
       }
     }
   }
   return $roles;
 }
-*/
+
 
 
 ?>
