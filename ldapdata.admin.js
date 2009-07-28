30655ba4ecfab0f649452910932a811e6ff74a0e
Miglius Alaburda <miglius@gmail.com>
2009-07-28 14:03:05 +0000

    ldap_integration:
    Changelog:
     * Moved AD comma escaping regexp from the groups module to the LDAPInterface search;
     * Implemented AJAX testing of the admin DN and password;

---
+++ ldapdata.admin.js
@@ -0,0 +1,19 @@
+// $Id$
+
+/**
+ * Behaviours are bound to the Drupal namespace.
+ */
+Drupal.behaviors.ldapdata = function(context) {
+  $('#edit-test').click(function(event) {
+    $('#test-message').hide();
+    $('#test-spinner').show();
+    var url = window.location.href + '/test';
+    $.post(url, { binddn: $('#edit-ldapdata-binddn').val(), bindpw: bindpw = $('#edit-ldapdata-bindpw').val(), bindpw_clear: bindpw_clear = $('#edit-ldapdata-bindpw-clear').val() },
+      function(data){
+        $('#test-spinner').hide();
+        $('#test-message').show().removeClass('status error').addClass(data.status ? 'status' : 'error').html(data.message);
+      }, "json");
+    return false;
+  });
+}
+
